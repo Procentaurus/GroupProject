9f8eff8517a4b35d3d1c6973564142f3cb4cadf1
Procentaurus <michalski.ob@gmail.com>
2023-10-23 11:01:12 +0200

    Added urls.py files to each module and added additional configuration to setting.py

---
+++ customUser/urls.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register')
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
