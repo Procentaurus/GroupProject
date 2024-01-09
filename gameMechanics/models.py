909261bf9cd0b4846f6ae22a3e6f0b29c8fd279a
Karol Sieniewicz <karolsieniewicz@gmail.com>
2024-01-09 14:15:10 +0100

    [MEC] Add basic mechanics models for cards and events/tasks
    Following basic mechanics - first iteration of card and event models

--- gameMechanics/models.py
+++ gameMechanics/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 
-class Card(models.Model):
-    pass
+class Entity(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=100)
 
-class ActionCard(Card):
-    pass
+class ActionCard(Entity):
+    damage_value = models.IntegerField()
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = 'Insult'
+        self.description = 'Insult your oponent for 20 damage.'
+        self.damage_value = 20
 
-class ReactionCard(Card):
-    pass
+class ReactionCard(Entity):
+    defense_value = models.IntegerField()
+    type_choices = [
+        'Brute',
+        'Influence',
+        'Inteligence'
+    ]
+    type = models.CharField(max_length=15, choices=type_choices)
 
-class Task(models.Model):
-    pass
+class Task(Entity):
+    arena_choices = [
+         'Hallway',
+         'Classroom',
+         'Canteen'
+    ]
+    arena = models.CharField(max_length=15, choices=arena_choices)
 
 class StudentTask(Task):
     pass
