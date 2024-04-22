07a8de763c0d0ff027575d98e82827b20bca2614
Procentaurus <michalski.ob@gmail.com>
2024-04-22 19:40:33 +0200

    [NET] Simplified routing

--- WebGame/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from gameNetworking.consumers import GameConsumer
-
-application = ProtocolTypeRouter({  # default routing for websocket component of application
-    "websocket": URLRouter([
-        path("ws/game/<str:conflict_side>/", GameConsumer.as_asgi()),
-    ]),
-})
--- gameNetworking/routing.py
+++ gameNetworking/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from .consumers import GameConsumer
 
 
-websocket_urlpatterns = [
+url_patterns = [
     re_path(r'^ws/game/(?P<conflict_side>\w+)/$', GameConsumer.as_asgi()),
 ]
