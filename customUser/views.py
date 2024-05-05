3be49984a8bd99c514263066e6d0002697f9c249
Procentaurus <michalski.ob@gmail.com>
2024-05-05 19:47:07 +0200

    [USR] Removed unnecessary import from views.py

--- customUser/views.py
+++ customUser/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
 import bleach
