1306bb287686f30fc6b3a1d115e6dcab90cca571
Procentaurus <michalski.ob@gmail.com>
2024-05-30 22:15:17 +0200

    [NET] Added deletion of redis messages when game timeout

--- gameNetworking/implementations/game_consumer_impl/disconnect.py
+++ gameNetworking/implementations/game_consumer_impl/disconnect.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 from ...models.queries import get_game
-from ...scheduler.scheduler import add_delayed_task
+from ...scheduler.scheduler import add_delayed_task, remove_delayed_task
 
 
 class Disconnector:
@@ -19,6 +19,7 @@ class Disconnector:
                 settings.DELETE_GAME_TIMEOUT,
                 settings.DELETE_GAME_TIMEOUT_FUNC
             )
+            self.remove_all_delayed_tasks()
         await self._remove_player_from_group(g_id)
  
     async def _send_game_end_info_to_opponent(self):
@@ -35,3 +36,13 @@ class Disconnector:
     async def _remove_player_from_group(self, game_id):
         await self._consumer.channel_layer.group_discard(
             f"game_{game_id}", self._consumer.channel_name)
+        
+    def remove_all_delayed_tasks(self, game):
+        teacher_id = game.teacher_player.id
+        student_id = game.student_player.id
+        remove_delayed_task(f'limit_action_time_{teacher_id}')
+        remove_delayed_task(f'limit_action_time_{student_id}')
+        remove_delayed_task(f'limit_reaction_time_{teacher_id}')
+        remove_delayed_task(f'limit_reaction_time_{student_id}')
+        remove_delayed_task(f'limit_hub_time_{teacher_id}')
+        remove_delayed_task(f'limit_hub_time_{student_id}')
