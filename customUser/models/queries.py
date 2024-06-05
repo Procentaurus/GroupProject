a25902ff41243859af287df26eccee9bd8a401bc
Piotrek Michalski <110058971+Procentaurus@users.noreply.github.com>
2024-06-05 16:32:34 +0200

    [NET] Fixed invalid game archive initialization (#75)

--- customUser/models/queries.py
+++ customUser/models/queries.py
@@ -5,13 +5,15 @@ from .game_archive.game_archive import GameArchive
 
 ### game_archive ###
 
-def create_game_archive(game, winner):
+async def create_game_archive(game, winner):
+    teacher_player = await (await game.get_teacher_player()).get_user()
+    student_player = await (await game.get_student_player()).get_user()
     archive = GameArchive.objects.create(
         start_date=game.start_datetime.date(),
         start_time=game.start_datetime.time(),
         winner=winner,
-        teacher_player=game.teacher_player.user,
-        student_player=game.student_player.user,
+        teacher_player=teacher_player,
+        student_player=student_player,
         length_in_sec = (datetime.now() - game.start_datetime).total_seconds()
     )
     archive.save()
