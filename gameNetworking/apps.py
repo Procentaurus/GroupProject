ec2f69fa7d80c4bec36d45dd67bc645b0fe7bd4d
Procentaurus <michalski.ob@gmail.com>
2024-08-23 17:23:31 +0200

    [NET] Restructured threading for delayed tasks

--- gameNetworking/apps.py
+++ gameNetworking/apps.py
@@ -1,18 +1,6 @@
-from WebGame.loggers import get_server_logger
-
-import threading
 from django.apps import AppConfig
-
-
-logger = get_server_logger()
 
 
 class GamenetworkingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gameNetworking'
-
-    def ready(self):
-        from .messager.scheduler import start_task_checker
-
-        threading.Thread(target=start_task_checker, daemon=True).start()
-        logger.info("Thread handling game tasks has started")
---
+++ gameNetworking/management/commands/run_delayed_tasks_thread.py
@@ -0,0 +1,28 @@
+import redis
+import threading
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from WebGame.loggers import get_server_logger
+
+from ...messager.scheduler import start_task_checker
+
+
+redis_client = redis.StrictRedis(
+    host=settings.REDIS_MESSAGING_HOST,
+    port=settings.REDIS_MESSAGING_PORT,
+    db=settings.REDIS_MESSAGING_DB
+)
+logger = get_server_logger()
+
+
+class Command(BaseCommand):
+    help = 'Start threads to provide Redis delayed game tasks handling'
+
+    def handle(self, *args, **kwargs):
+        tasks_thread = threading.Thread(
+            target=start_task_checker)
+
+        tasks_thread.start()
+        logger.info("Thread handling game tasks has started")
+        tasks_thread.join()
