96279bc90dfaa0f22575a410406ba87c8e637951
Piotrek Michalski <110058971+Procentaurus@users.noreply.github.com>
2024-09-18 19:19:44 +0200

    Added Nginx to backend (#96)
    * Added Nginx to backend
    
    * Moved cards photos to SocketComponent
    
    * Fixed nginx configuration
    
    * [NET] Added game token deleting

--- SocketComponent/SocketComponent/urls.py
+++ SocketComponent/SocketComponent/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('socket_admin/', admin.site.urls),
     path('game/', include('gameNetworking.urls'))
 ]
