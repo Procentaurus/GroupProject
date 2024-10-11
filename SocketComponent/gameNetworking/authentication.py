4e858046f1495be30f0f3d33dd7e8b5d46227a03
Procentaurus <michalski.ob@gmail.com>
2024-10-11 11:19:30 +0200

    [NET] Fixed 500 server error for non auth users' requests

--- SocketComponent/gameNetworking/authentication.py
+++ SocketComponent/gameNetworking/authentication.py
@@ -7,11 +7,24 @@ from rest_framework_simplejwt.settings import api_settings
 class NonUserJWTAuthentication(JWTAuthentication):
 
     def get_user(self, validated_token: Token):
-        user_id = None
         try:
-            user_id = validated_token[api_settings.USER_ID_CLAIM]
+            user_id = validated_token.get(api_settings.USER_ID_CLAIM)
             is_admin = validated_token.get('is_admin', False)
+            if not user_id:
+                raise InvalidToken("Invalid token") 
+            else: return (user_id, is_admin)
         except KeyError:
             raise InvalidToken(
-                ("Token contained no recognizable user identification"))
-        return (user_id, is_admin)
+                "Token contained no recognizable user identification.")
+
+    def authenticate(self, request):
+        header = self.get_header(request)
+        if header is None:
+            return None
+
+        raw_token = self.get_raw_token(header)
+        if raw_token is None:
+            return None
+
+        validated_token = self.get_validated_token(raw_token)
+        return (self.get_user(validated_token), None)
